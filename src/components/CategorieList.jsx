a9c7f6eebd0cb6fc96022f3f46be19c80e3b94c0
ezio <eziopborges@gmail.com>
2022-11-10 20:50:17 -0300

    6º requisito

--- src/components/CategorieList.jsx
+++ src/components/CategorieList.jsx
@@ -4,39 +4,57 @@ import * as api from '../services/api';
 class CategorieList extends React.Component {
   state = {
     categories: [],
+    produto: [],
   };
 
   componentDidMount() {
     this.fetchCategories();
+    this.getProduct();
   }
 
   fetchCategories = async () => {
     const categories = await api.getCategories();
+
     this.setState({
       categories,
     });
   };
 
+  getProduct = async (categoryId) => {
+    const produtos = await api.getProductById(categoryId);
+    console.log('prod', produtos.results);
+
+    this.setState({
+      produto: produtos.results,
+    });
+  };
+
   render() {
-    const { categories } = this.state;
+    const { categories, produto } = this.state;
     return (
-      <div className="categories">
-        <p className="category-title">Categorias:</p>
-        { categories.map((category) => (
-          <label
-            htmlFor={ category.name }
-            data-testid="category"
-            key={ category.id }
-          >
+      <div>
+        <p>Categorias:</p>
+        {categories.map((category) => (
+          <label htmlFor="category" data-testid="category" key={ category.id }>
             <input
               type="radio"
               name="category"
-              className="category-items"
               id={ category.name }
               value={ category.name }
+              onClick={ () => this.getProduct(category.id) }
             />
-            { category.name }
-          </label>)) }
+            {category.name}
+          </label>
+        ))}
+        <div>
+          {produto.map((prod, id) => (
+            <div key={ id } data-testid="product" className="produtos">
+              <p>{prod.title}</p>
+              <img src={ prod.thumbnail } alt={ prod.title } />
+              {prod.title}
+            </div>
+          ))}
+        </div>
       </div>
     );
   }
--- src/services/api.js
+++ src/services/api.js
@@ -5,12 +5,16 @@ export async function getCategories() {
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  console.log(categoryId);
   const request = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
   const requestJson = await request.json();
   return requestJson;
 }
 
-export async function getProductById() {
+export async function getProductById(id) {
   // Esta implementação específica não é avaliada, mas pode ajudar você 🙂
   // Atenção: essa função não deverá ser chamada na tela do carrinho de compras.
+  const request = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${id}`);
+  const requestJson = await request.json();
+  return requestJson;
 }
